8f037dbe8569926d3b61b1be8556dadd103af078
jdoorn <44007978+jdoorn@users.noreply.github.com>
2018-11-26 17:05:22 -0700

    Survey files

---
+++ menu.php
@@ -0,0 +1,16 @@
+<!-- menu.php -->
+
+
+<nav>
+    <ul>
+ <li>   <a href="default.php">Answer Questions</a> </li>
+ <li>   <a href="Display_Edit_a.php">Display Answers</a> </li>
+ <li>   <a href="Display_Edit_a_graph.php">Bar Chart</a> </li>
+ <li>   <a href="Display_Edit_a_pie.php">Pie Chart</a> </li>
+</ul>
+</nav>
+
+
+<?php
+
+?>
